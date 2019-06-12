ecfb0ae80856343f1161f1122f8f00181d7aa21a
maggioni <maggioni@intre.it>
2019-06-12 12:40:07 +0200

    Add log message

--- cps/web.py
+++ cps/web.py
@@ -2331,6 +2331,7 @@ def login():
     if request.method == "POST":
         form = request.form.to_dict()
         user = ub.session.query(ub.User).filter(func.lower(ub.User.nickname) == form['username'].strip().lower()).first()
+        app.logger.info('---->' + str(user.nickname))
         if ldap_support and config.config_use_ldap and user and user.nickname is not "admin":
             try:
               if ldap.bind_user(form['username'], form['password']) is not None:
