526e09897a53a6a1b2c08d9ac4a70f18b9ef1739
maggioni <maggioni@intre.it>
2019-06-13 12:00:46 +0200

    Removed workaround for admin user

--- cps/web.py
+++ cps/web.py
@@ -2328,7 +2328,7 @@ def login():
     if request.method == "POST":
         form = request.form.to_dict()
         user = ub.session.query(ub.User).filter(func.lower(ub.User.nickname) == form['username'].strip().lower()).first()
-        if ldap_support and config.config_use_ldap and user and user.nickname != "admin":
+        if ldap_support and config.config_use_ldap and user:
             try:
               if ldap.bind_user(form['username'], form['password']) is not None:
                    login_user(user, remember=True)
@@ -2340,7 +2340,7 @@ def login():
             except Exception as exception:
                 app.logger.error( 'LDAP Exception: ' + str(exception))
                 flash(_(u"Generic error occurred during LDAP login"), category="error")
-        elif user and (user.nickname == "admin" or check_password_hash(user.password, form['password'])) and user.nickname is not "Guest":
+        elif user and check_password_hash(user.password, form['password'])V and user.nickname is not "Guest":
             login_user(user, remember=True)
             flash(_(u"you are now logged in as: '%(nickname)s'", nickname=user.nickname), category="success")
             return redirect_back(url_for("index"))
